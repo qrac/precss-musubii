bf53b89b6a8941d6ecd19d857f7ac0855d10f58b
Qrac <qrac.jp@gmail.com>
2020-03-04 13:13:37 +0900

    Add demo pre

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,31 @@
+import React from "react"
+import beautify from "js-beautify"
+
 import AppHead from "~/components/commons/app-head"
+import DemoPre from "~/components/parts/demo-pre"
+
+const beautifyHtmlOptions = {
+  inline: ["i"],
+  indent_size: 2
+}
 
 export const meta = {}
 
-export default () => (
-  <div className="example">
-    <AppHead meta={meta} />
-    Hello World!
-  </div>
-)
+export class PreviewBasicMarkup extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+  render() {
+    const contents = `npm install precss-musubii`
+    const formattedCode = beautify.html(contents, beautifyHtmlOptions)
+    return (
+      <article className="bl_page">
+        <AppHead meta={meta} />
+        <DemoPre language="" code={formattedCode} />
+      </article>
+    )
+  }
+}
+
+export default PreviewBasicMarkup
