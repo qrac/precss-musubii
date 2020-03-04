e1b9c300eb12f190a1ca89b6f3160ed9321e8cf3
Qrac <qrac.jp@gmail.com>
2020-03-04 13:36:06 +0900

    Fix demo nav

--- src/components/commons/app-nav.js
+++ src/components/commons/app-nav.js
@@ -10,19 +10,43 @@ export default () => {
     <nav className="ly_sect ly_sect_nav">
       <div className="ly_sect_inner ly_sect_inner__desk_max_w_tab hp_pd_horiz_md">
         <ul className="bl_nav_btns bl_nav_btns__c">
-          {pjt.navs.map((nav, i) => (
-            <li className="bl_nav_item" key={i}>
-              <Link href={nav.path}>
-                <a
-                  className={
-                    path === nav.path ? "el_nav_btn is_active" : "el_nav_btn"
-                  }
-                >
-                  {nav.title}
-                </a>
-              </Link>
-            </li>
-          ))}
+          {pjt.navs.map((nav, i) =>
+            (() => {
+              if (nav.path === "/") {
+                return (
+                  <li className="bl_nav_item" key={i}>
+                    <Link href={nav.path}>
+                      <a
+                        className={
+                          path === nav.path
+                            ? "el_nav_btn is_active"
+                            : "el_nav_btn"
+                        }
+                      >
+                        {nav.title}
+                      </a>
+                    </Link>
+                  </li>
+                )
+              } else {
+                return (
+                  <li className="bl_nav_item" key={i}>
+                    <Link href={nav.path}>
+                      <a
+                        className={
+                          path === nav.path
+                            ? "el_nav_btn is_active"
+                            : "el_nav_btn"
+                        }
+                      >
+                        {nav.title}
+                      </a>
+                    </Link>
+                  </li>
+                )
+              }
+            })()
+          )}
         </ul>
       </div>
     </nav>
