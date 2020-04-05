977248316be2234bdb40cfedc693c8891a69cc37
Qrac <qrac.jp@gmail.com>
2020-04-05 16:41:04 +0900

    Fix demo

--- src/components/previews/preview-grid.js
+++ src/components/previews/preview-grid.js
@@ -19,7 +19,7 @@ const columnNums = [
   { id: 9, text: "9", value: 9 },
   { id: 10, text: "10", value: 10 },
   { id: 11, text: "11", value: 11 },
-  { id: 12, text: "12", value: 12 }
+  { id: 12, text: "12", value: 12 },
 ]
 
 const columnSizes = [
@@ -36,7 +36,7 @@ const columnSizes = [
   { id: 9, text: "9/12幅", value: "hp_fx9" },
   { id: 10, text: "10/12幅", value: "hp_fx10" },
   { id: 11, text: "11/12幅", value: "hp_fx11" },
-  { id: 12, text: "12/12幅", value: "hp_fx12" }
+  { id: 12, text: "12/12幅", value: "hp_fx12" },
 ]
 
 const gaps = [
@@ -47,7 +47,7 @@ const gaps = [
   { id: 4, text: "Medium", value: "md" },
   { id: 3, text: "Large", value: "lg" },
   { id: 2, text: "XL", value: "xl" },
-  { id: 1, text: "XXL", value: "xxl" }
+  { id: 1, text: "XXL", value: "xxl" },
 ]
 
 const aligns = [
@@ -58,12 +58,12 @@ const aligns = [
   { id: 4, text: "Center", value: "ly_grid__c" },
   { id: 5, text: "Right", value: "ly_grid__r" },
   { id: 6, text: "Between", value: "ly_grid__between" },
-  { id: 7, text: "Around", value: "ly_grid__around" }
+  { id: 7, text: "Around", value: "ly_grid__around" },
 ]
 
 const beautifyHtmlOptions = {
   inline: ["i"],
-  indent_size: 2
+  indent_size: 2,
 }
 
 export class PreviewGridBasic extends React.Component {
@@ -71,7 +71,7 @@ export class PreviewGridBasic extends React.Component {
     super(props)
     this.state = {
       columnNum: 3,
-      columnSize: "hp_fx0"
+      columnSize: "hp_fx0",
     }
     this.changeColumnNum = this.changeColumnNum.bind(this)
     this.changeColumnSize = this.changeColumnSize.bind(this)
@@ -90,8 +90,9 @@ export class PreviewGridBasic extends React.Component {
     const columns = []
     for (let i = 0; i < columnNum; i++) {
       columns.push(
-        `<div class="ly_grid_col ${columnSize}"><div>children ${i +
-          1}</div></div>`
+        `<div class="ly_grid_col ${columnSize}"><div>children ${
+          i + 1
+        }</div></div>`
       )
     }
     const contents = (gridTagBefore + columns.join("") + gridTagAfter)
@@ -105,14 +106,14 @@ export class PreviewGridBasic extends React.Component {
             <DemoOption title={"Columns"}>
               <DemoOptionBoxRadios
                 patterns={columnNums}
-                parentChange={value => this.changeColumnNum(value)}
+                parentChange={(value) => this.changeColumnNum(value)}
                 checked={this.state.columnNum}
               />
             </DemoOption>
             <DemoOption title={"Flex"}>
               <DemoOptionBoxRadios
                 patterns={columnSizes}
-                parentChange={value => this.changeColumnSize(value)}
+                parentChange={(value) => this.changeColumnSize(value)}
                 checked={this.state.columnSize}
               />
             </DemoOption>
@@ -136,7 +137,7 @@ export class PreviewGridGap extends React.Component {
     this.state = {
       columnNum: 4,
       columnSize: "hp_fx6",
-      gap: "md"
+      gap: "md",
     }
     this.changeColumnNum = this.changeColumnNum.bind(this)
     this.changeColumnSize = this.changeColumnSize.bind(this)
@@ -161,8 +162,9 @@ export class PreviewGridGap extends React.Component {
     const columns = []
     for (let i = 0; i < columnNum; i++) {
       columns.push(
-        `<div class="ly_grid_col ${columnSize}"><div>children ${i +
-          1}</div></div>`
+        `<div class="ly_grid_col ${columnSize}"><div>children ${
+          i + 1
+        }</div></div>`
       )
     }
     const contents = (gridTagBefore + columns.join("") + gridTagAfter)
@@ -176,7 +178,7 @@ export class PreviewGridGap extends React.Component {
             <DemoOption title={"Gap"}>
               <DemoOptionBoxRadios
                 patterns={gaps}
-                parentChange={value => this.changeGap(value)}
+                parentChange={(value) => this.changeGap(value)}
                 checked={this.state.gap}
               />
             </DemoOption>
@@ -200,7 +202,7 @@ export class PreviewGridGapVertical extends React.Component {
     this.state = {
       columnNum: 3,
       columnSize: "hp_fx12",
-      gap: "md"
+      gap: "md",
     }
     this.changeColumnNum = this.changeColumnNum.bind(this)
     this.changeColumnSize = this.changeColumnSize.bind(this)
@@ -225,8 +227,9 @@ export class PreviewGridGapVertical extends React.Component {
     const columns = []
     for (let i = 0; i < columnNum; i++) {
       columns.push(
-        `<div class="ly_grid_col ${columnSize}"><div>children ${i +
-          1}</div></div>`
+        `<div class="ly_grid_col ${columnSize}"><div>children ${
+          i + 1
+        }</div></div>`
       )
     }
     const contents = (gridTagBefore + columns.join("") + gridTagAfter)
@@ -240,7 +243,7 @@ export class PreviewGridGapVertical extends React.Component {
             <DemoOption title={"Gap Vertical"}>
               <DemoOptionBoxRadios
                 patterns={gaps}
-                parentChange={value => this.changeGap(value)}
+                parentChange={(value) => this.changeGap(value)}
                 checked={this.state.gap}
               />
             </DemoOption>
@@ -264,7 +267,7 @@ export class PreviewGridGapHorizontal extends React.Component {
     this.state = {
       columnNum: 3,
       columnSize: "hp_fx0",
-      gap: "md"
+      gap: "md",
     }
     this.changeColumnNum = this.changeColumnNum.bind(this)
     this.changeColumnSize = this.changeColumnSize.bind(this)
@@ -289,8 +292,9 @@ export class PreviewGridGapHorizontal extends React.Component {
     const columns = []
     for (let i = 0; i < columnNum; i++) {
       columns.push(
-        `<div class="ly_grid_col ${columnSize}"><div>children ${i +
-          1}</div></div>`
+        `<div class="ly_grid_col ${columnSize}"><div>children ${
+          i + 1
+        }</div></div>`
       )
     }
     const contents = (gridTagBefore + columns.join("") + gridTagAfter)
@@ -304,7 +308,7 @@ export class PreviewGridGapHorizontal extends React.Component {
             <DemoOption title={"Gap Horizontal"}>
               <DemoOptionBoxRadios
                 patterns={gaps}
-                parentChange={value => this.changeGap(value)}
+                parentChange={(value) => this.changeGap(value)}
                 checked={this.state.gap}
               />
             </DemoOption>
@@ -327,7 +331,7 @@ export class PreviewGridAlign extends React.Component {
     super(props)
     this.state = {
       columnNum: 4,
-      align: ""
+      align: "",
     }
     this.changeColumnNum = this.changeColumnNum.bind(this)
     this.changeAlign = this.changeAlign.bind(this)
@@ -361,7 +365,7 @@ export class PreviewGridAlign extends React.Component {
             <DemoOption title={"Align"}>
               <DemoOptionBoxRadios
                 patterns={aligns}
-                parentChange={value => this.changeAlign(value)}
+                parentChange={(value) => this.changeAlign(value)}
                 checked={this.state.align}
               />
             </DemoOption>
@@ -384,7 +388,7 @@ export class PreviewGridReverse extends React.Component {
     super(props)
     this.state = {
       reverse: true,
-      columnNum: 4
+      columnNum: 4,
     }
     this.toggleReverse = this.toggleReverse.bind(this)
   }
@@ -436,7 +440,6 @@ export class PreviewGridStretch extends React.Component {
     super(props)
     this.state = {
       stretch: true,
-      columnNum: 4
     }
     this.toggleStretch = this.toggleStretch.bind(this)
   }
@@ -445,16 +448,18 @@ export class PreviewGridStretch extends React.Component {
   }
   render() {
     const stretch = this.state.stretch ? "ly_grid__stretch" : ""
-    const columnNum = this.state.columnNum
     const gridTagBefore = `<div class="ly_grid ly_grid__gap_md ${stretch}">`
     const gridTagAfter = `</div>`
-    const columns = []
-    for (let i = 0; i < columnNum; i++) {
-      columns.push(
-        `<div class="ly_grid_col"><div>children ${i + 1}</div></div>`
-      )
-    }
-    const contents = (gridTagBefore + columns.join("") + gridTagAfter)
+    const columns = `<div class="ly_grid_col">
+      <div><p>children 1</p></div>
+    </div>
+    <div class="ly_grid_col">
+      <div><p>children 2</p><p>children 2</p></div>
+    </div>
+    <div class="ly_grid_col">
+      <div><p>children 3</p><p>children 3</p><p>children 3</p></div>
+    </div>`
+    const contents = (gridTagBefore + columns + gridTagAfter)
       .replace(/\s+/g, " ")
       .replace(/\s\"/g, '"')
     const formattedCode = beautify.html(contents, beautifyHtmlOptions)
